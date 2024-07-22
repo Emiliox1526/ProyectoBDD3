88c0f4f04ef96448a6cc87533e0751b078116a83
josvi <josvi@LAPTOP-4C6TQ3GT>
2024-07-21 20:54:48 -0400

    Commit de Conexion

--- ProyectoFinal/src/Conexion/SQL.java
+++ ProyectoFinal/src/Conexion/SQL.java
@@ -6,17 +6,19 @@ import java.sql.SQLException;
 
 public class SQL {
     
-    private static final String DB_URL = "jdbc:sqlserver://STLAB-DB01:1433;databaseName=Asignacion Final - EJGR0001;integratedSecurity=true;";
+    private static final String DB_URL = "jdbc:sqlserver://(localdb)\\JRod:1433;databaseName=Academico;integratedSecurity=true;";
 
-    public static Connection getConnection() throws ClassNotFoundException {
+    public static Connection getConnection() {
         Connection connection = null;
         try {
-            
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(DB_URL);
             System.out.println("Conectado*");
         } catch (SQLException e) {
             System.out.println("Error entrando en la base de datos");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error cargando el driver de la base de datos");
             e.printStackTrace();
         }
         return connection;
