557d224951076974d23db3b6cd0601d71942d754
josvi <josvi@LAPTOP-048K0K4K>
2024-07-24 18:41:37 -0400

    a

--- ProyectoFinal/src/logico/Cliente.java
+++ ProyectoFinal/src/logico/Cliente.java
@@ -3,7 +3,7 @@ package logico;
 import java.io.Serializable;
 
 public class Cliente implements Serializable{
-	
+	//aaaaaaaaa
 
 	private static final long serialVersionUID = 1L;
 	private String id;
